86c8318a44d920c68fd0bd0eb92bc700eb639226
ARod88 <alma.rodriguez72@yahoo.com>
2023-09-17 13:51:51 -0700

    succesful connection to mongo db

--- final-project/backend/db/config.js
+++ final-project/backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/custom-rug-website")
+mongoose.connect("mongodb://127.0.0.1/custom-rug-website")
--- final-project/backend/index.js
+++ final-project/backend/index.js
@@ -2,10 +2,17 @@ const express = require('express');
 require('./db/config')
 // const cors = require('cors');
 // const bodyParser = require('body-parser');
-
+const User = require('./db/User');
 const app = express();
 
+//middleware
+app.use(express.json());
 
+app.post("/register", async (req, res) => {
+    let user = new User(req.body);
+    let result =  await user.save();
+    res.send(req.body)
+})
 
 app.listen(5000)
 
