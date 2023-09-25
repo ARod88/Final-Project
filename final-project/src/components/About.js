c7cc4d37e4b6e4405288142659146d8fed5c22af
ARod88 <alma.rodriguez72@yahoo.com>
2023-09-24 22:53:10 -0700

    added media query

--- final-project/src/components/About.js
+++ final-project/src/components/About.js
@@ -2,10 +2,23 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { styled } from 'styled-components';
+
+const AboutContainer = styled(Container)`
+
+
+  @media (max-width: 768px) {
+
+    .card-image {
+        position: fixed;
+        
+
+  }
+`;
 
 const About = () => {
     return (
-        <Container className="aboutpage">
+        <AboutContainer className="aboutpage">
             <Container>
                 <h1 className='artist'>The Artist</h1>
             </Container>
@@ -16,23 +29,23 @@ const About = () => {
                     </Col>
                     <Col md={8}> 
                         <Card.Body>
-                            <Card.Text>
+                            <Card.Text className='text'>
                                 Behind the creater of the custom rugs, is a story of an Angelino.
                             </Card.Text>
-                            <Card.Text>
+                            <Card.Text className='text'>
                                 Sergio Olvera is a local carpetmaker from our very own Los Angeles, CA.
                             </Card.Text>
-                            <Card.Text>
+                            <Card.Text className='text'>
                                 Like every average Angelino, we are always trying to find ways to hustle.
                             </Card.Text>
-                            <Card.Text>
+                            <Card.Text className='text'>
                                 He had an idea, set his mind to it, believed in himself, taught himself how to tuft rugs, and started a custom rug business on Instagram.
                             </Card.Text>
                         </Card.Body>
                     </Col>
                 </Row>
             </Card>
-        </Container>
+        </AboutContainer>
     );
 }
 
